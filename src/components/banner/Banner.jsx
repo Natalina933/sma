050c4f57af614385a1aa8f2e1fd353995a9281d5
NathHome <nat933@free.fr>
2023-08-20 15:03:28 +0200

    modification banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 import styles from './banner.module.css';
 import Hero from 'public/hero.jpg';
 import Image from 'next/image';
-
 const Banner = () => {
   return (
     <section className={styles.banner}>
       <div className={styles.overlay}></div>
       <div className={styles.textContainer}>
-        <div className={styles.leftText}>
+        <div className={styles.text}>
           <h2>Des moments inoubliables</h2>
-          <p>Explorez nos activités passionnantes et rejoignez nous.</p>
+          <p>Explorez nos activités passionnantes et rejoignez-nous.</p>
         </div>
-        <div className={styles.imgText}>
-          <div className={styles.img}>
-            <Image
-              src={Hero}
-              width={300} 
-              height={200} 
-              style={{ borderRadius: '15px' }} 
-              alt="Activités passionnantes" />
-          </div>
+        <div className={styles.img}>
+          <Image
+            src={Hero}
+            layout="fill" /* Remplit l'espace avec l'image */
+            objectFit="cover" /* Ajuste l'image pour couvrir le conteneur */
+            style={{ borderRadius: '15px' }} /* Utilisez les doubles accolades pour les styles en ligne */
+            alt="Activités passionnantes"
+          />
         </div>
       </div>
     </section>
