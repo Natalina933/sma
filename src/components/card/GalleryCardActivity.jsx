49539b06f6bc40a7debc2e6c8251aff185ac3fa8
Nathalie <nat933@free.fr>
2023-08-28 19:19:07 +0200

    correction gallery

--- src/components/card/GalleryCardActivity.jsx
+++ src/components/card/GalleryCardActivity.jsx
@@ -1,48 +1,48 @@
-/* use client */
-import React, { useState } from 'react';
-import Image from 'next/image';
-import styles from './galleryCardActivity.module.css';
+// /* use client */
+// import React, { useState } from 'react';
+// import Image from 'next/image';
+// import styles from './galleryCardActivity.module.css';
 
-const GalleryCardActivity = ({ activityData }) => {
-  const [expandedActivity, setExpandedActivity] = useState(null);
+// const GalleryCardActivity = ({ activityData }) => {
+//   const [expandedActivity, setExpandedActivity] = useState(null);
 
-  const handleToggleDescription = (activityId) => {
-    setExpandedActivity(prevExpandedActivity =>
-      prevExpandedActivity === activityId ? null : activityId
-    );
-  };
+//   const handleToggleDescription = (activityId) => {
+//     setExpandedActivity(prevExpandedActivity =>
+//       prevExpandedActivity === activityId ? null : activityId
+//     );
+//   };
 
-  return (
-    <div className={styles.gallery}>
-      {activityData.map(activity => (
-        <div
-          className={`${styles.card} ${expandedActivity === activity.id ? styles.cardExpanded : ''}`}
-          key={activity.id}
-        >
-          <Image src={activity.image} width={300} height={130} alt={activity.title} />
-          <div className={styles.cardContent}>
-            <h2 className={styles.activityTitle}>{activity.title}</h2>
-            <p className={styles.description}>{activity.description}</p>
+//   return (
+//     <div className={styles.gallery}>
+//       {activityData.map(activity => (
+//         <div
+//           className={`${styles.card} ${expandedActivity === activity.id ? styles.cardExpanded : ''}`}
+//           key={activity.id}
+//         >
+//           <Image src={activity.image} width={300} height={130} alt={activity.title} />
+//           <div className={styles.cardContent}>
+//             <h2 className={styles.activityTitle}>{activity.title}</h2>
+//             <p className={styles.description}>{activity.description}</p>
 
-            <div className={styles.details}>
-              <p className={styles.schedule}>{activity.schedule}</p>
-              <p className={styles.place}>{activity.place}</p>
-              <p className={styles.price}>
-                {activity.price === "gratuit" ? "Gratuit" : `${activity.price} €`}
-              </p>
-              <button
-                className={styles.addButton}
-                type="button"
-                onClick={() => handleToggleDescription(activity.id)}
-              >
-                {expandedActivity === activity.id ? 'Réduire' : 'En savoir plus'}
-              </button>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+//             <div className={styles.details}>
+//               <p className={styles.schedule}>{activity.schedule}</p>
+//               <p className={styles.place}>{activity.place}</p>
+//               <p className={styles.price}>
+//                 {activity.price === "gratuit" ? "Gratuit" : `${activity.price} €`}
+//               </p>
+//               <button
+//                 className={styles.addButton}
+//                 type="button"
+//                 onClick={() => handleToggleDescription(activity.id)}
+//               >
+//                 {expandedActivity === activity.id ? 'Réduire' : 'En savoir plus'}
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default GalleryCardActivity;
+// export default GalleryCardActivity;
