44ce5425a5d0592aaab3095295cd0bfafc73b882
NathHome <nat933@free.fr>
2023-10-09 21:56:19 +0200

    correcction css : activity et contact page

--- src/app/activity/page.jsx
+++ src/app/activity/page.jsx
@@ -8,13 +8,16 @@ const Activity = () => {
       <h1 className={styles.selectTitile}>Nos Activités</h1>
       <div className={styles.items}>
         <Link href="/activity/illustrations" className={styles.item}>
-          <span className={styles.title}>Illustrations</span>
+          <span className={styles.title}
+          >En plein air </span>
         </Link>
         <Link href="/activity/websites" className={styles.item}>
-          <span className={styles.title}>websites</span>
+          <span className={styles.title}
+          >Aquatique</span>
         </Link>
         <Link href="/activity/application" className={styles.item}>
-          <span className={styles.title}>Application</span>
+          <span className={styles.title}
+          >Théâtre</span>
         </Link>
       </div>
     </div>
