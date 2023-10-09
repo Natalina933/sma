a3ce1b3303d2d9da37b60e3404ac74c53bf6fd18
Nathalie <nat933@free.fr>
2023-10-09 17:44:48 +0200

    creation : page activity

---
+++ src/app/activity/layout.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css"
+const Layout = ({children}) => {
+    return (
+        <div>
+            <h1 className={styles.mainTitle}>Nos Activités cette année</h1>
+            {children}
+        </div>
+    )
+}
+
+export default Layout
--- src/app/activity/page.jsx
+++ src/app/activity/page.jsx
@@ -1,10 +1,24 @@
 /*user client*/
-import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const Activity = () => {
   return (
-    <div className={styles.container}>Activity</div>
+    <div className={styles.container}>
+      <h1 className={styles.selectTitile}>Nos Activités</h1>
+      <div className={styles.items}>
+        <Link href="/activity/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/activity/websites" className={styles.item}>
+          <span className={styles.title}>websites</span>
+        </Link>
+        <Link href="/activity/application" className={styles.item}>
+          <span className={styles.title}>Application</span>
+        </Link>
+
+      </div>
+    </div>
   )
 }
 
