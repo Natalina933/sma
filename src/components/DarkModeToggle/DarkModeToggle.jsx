65524afe3f83976382fcd9c98eba1031f01d6c20
Nathalie <nat933@free.fr>
2023-10-12 10:10:08 +0200

    modif icone darkMode

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,13 +1,17 @@
 import styles from "./darkModeToggle.module.css";
 import { useContext } from "react";
 import { ThemeContext } from "../../../context/ThemeContext";
-
+import {
+    FaHome,
+    FaCogs,
+   
+} from "react-icons/fa";
 const DarkModeToggle = () => {
     const { toggle, mode } = useContext(ThemeContext);
     return (
         <div className={styles.container} onClick={toggle}>
-            <div className={styles.icon}>🌙</div>
-            <div className={styles.icon}>🔆</div>
+            <div className={styles.icon}><FaCogs /></div>
+            <div className={styles.icon}><FaHome /></div>
             <div
                 className={styles.ball}
                 style={mode === "light" ? { left: "2px" } : { right: "2px" }}
