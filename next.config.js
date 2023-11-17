8e2b7dc5e0df5513ce79fe4ed111fd243c235c4a
NathHome <nat933@free.fr>
2023-11-17 19:46:32 +0100

    config : next.config vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+const nextConfig = {
+    experimental: {
+      // experimental options here
+    },
+    webpack: (config, { isServer }) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@/pages': path.resolve(__dirname, 'src/pages'),
+        '@/modules': path.resolve(__dirname, 'src/modules'),
+      };
+      return config;
+    },
+  };
