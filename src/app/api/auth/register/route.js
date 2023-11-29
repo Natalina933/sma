9e6c2af55a971aade9f89dd9ba05d73d8ae050bc
NathHome <nat933@free.fr>
2023-11-29 21:47:41 +0100

    ajout : log route register

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -4,38 +4,43 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
 
+const SALT_ROUNDS = 10; 
+
 export const POST = async (request) => {
     try {
         const { name, email, password } = await request.json();
-        // si l'utilisateur existe déjà dans la base de données
-        await connect(); // Connection à la base de données
+        
+        console.log("Tentative de création d'utilisateur...");
+        
+        await connect(); // Connexion à la base de données
 
-        //  si l'utilisateur existe déjà dans la base de données
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
+            console.log("Cet utilisateur existe déjà dans la base de données");
             return new NextResponse("Cet utilisateur existe déjà", {
                 status: 400, // Utilisateur déjà existant
             });
         }
 
-        // Hash du mot de passe avant de l'enregistrer dans la base de données
-        const hashedPassword = await bcrypt.hash(password, 10); // Utilise une valeur de salage plus élevée
-
-        // Création d'un nouvel utilisateur avec le mot de passe haché
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+        
         const newUser = new User({
             name,
             email,
             password: hashedPassword,
         });
 
-        // Enregistre le nouvel utilisateur dans la base de données
         await newUser.save();
 
+        console.log("Utilisateur créé avec succès");
+        
         return new NextResponse("Utilisateur créé", {
             status: 201, // Utilisateur créé avec succès
         });
     } catch (error) {
+        console.error("Erreur lors de la création de l'utilisateur :", error);
+        
         return new NextResponse(error.message, {
             status: 500, // Erreur serveur
         });
