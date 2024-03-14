c2a0efd0695dc7e68858a84462322a2150e092ac
Nathalie <nat933@free.fr>
2024-03-14 17:27:44 +0100

    amélioration erreur

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-//connexion à mongoose
+
+// Connexion à MongoDB
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
         console.log("Connecté à MongoDB");
     } catch (error) {
-        throw new Error("Connexion échouée!");//connexion échouée
+        // En cas d'erreur lors de la connexion à MongoDB, lancez une exception avec un message d'erreur
+        throw new Error("Connexion à MongoDB échouée : " + error.message);
     }
 };
+
 export default connect;
